0bd928e5dfd00bff716177324fc67034895b11a4
JunBeomHan <2hanjunbum6@gmail.com>
2023-04-09 21:16:19 +0900

    Add lexer_test.go

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,10 +1,8 @@
 package lexer
 
 import (
-	"fmt"
+	"monkey_clone/token"
 	"testing"
-
-	"../token"
 )
 
 func TestNextToken(t *testing.T) {
@@ -29,25 +27,11 @@ func TestNextToken(t *testing.T) {
 
 	for index, resTok := range tests {
 		tok := l.NextToken()
-		if token.TokenType(tok.Type) != resTok.expectedType {
+		if tok.Type != resTok.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", index, resTok.expectedType, tok.Type)
 		}
 		if tok.Literal != resTok.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", index, resTok.expectedLiteral, tok.Literal)
 		}
-	}
-}
-
-func TestToeknEqual(t *testing.T) {
-	var tok token.Token
-	var tok1 token.TokenType
-
-	tok.Type = token.ASSIGN
-	tok1 = token.ASSIGN
-
-	if tok.Type != tok1 {
-		fmt.Println("다르다")
-	} else {
-		fmt.Println("같다.")
 	}
 }
